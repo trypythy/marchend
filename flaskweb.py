b6f33351e011e45e6447443d79adb7c633d8302a
trypythy <brijeshtripathi@Brijeshs-MacBook-Pro.local>
2019-03-30 03:18:01 +0530

    part 3, forms

--- flaskweb.py
+++ flaskweb.py
@@ -1,6 +1,9 @@
 # flaskweb.py
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'edc163356e4062185ed45a71c376d95c'
 
 reviews = [
 	{
@@ -20,13 +23,31 @@ reviews = [
 
 @app.route('/')
 @app.route('/home')
-def hello():
+def home():
 	return render_template('home.html', reviews=reviews)
 
 @app.route('/about')
 def about():
 	return render_template('about.html', title='About Us')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Yay! Account created successfully for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'bt@bt.com' and form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check username and password', 'danger')
+	return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
