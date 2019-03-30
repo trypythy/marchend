7d14a7134f97d08993236c10aad5f3a06cac7099
trypythy <brijeshtripathi@Brijeshs-MacBook-Pro.local>
2019-03-30 16:20:51 +0530

    chapter 4 - database created

--- flaskweb.py
+++ flaskweb.py
@@ -1,9 +1,38 @@
 # flaskweb.py
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'edc163356e4062185ed45a71c376d95c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	"""user model"""
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(100), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+	reviews = db.relationship('Review', backref='author', lazy=True)
+
+	def __repr__(self):
+		return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Review(db.Model):
+	"""Review model"""
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+	def __repr__(self):
+		return f"Review('{self.title}', '{self.date_posted}')"
+	
+
 
 reviews = [
 	{
