aab653761ed66f5c8f3e0fe646eabef36e6bb543
trypythy <brijeshtripathi@Brijeshs-MacBook-Pro.local>
2019-04-08 21:40:15 +0530

    chapter 7, a lot of image, user account related tasks

--- flaskweb/forms.py
+++ flaskweb/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskweb import db
 from flaskweb.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
 	"""form for registration"""
@@ -27,3 +29,21 @@ class LoginForm(FlaskForm):
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+	"""form for update account view"""
+	username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	submit = SubmitField('Update')
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if username.data != current_user.username:
+			if user:
+				raise ValidationError("This username exists already, please choose another one")
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if email.data != current_user.email:
+			if user:
+				raise ValidationError("This email exists, please choose another one")
